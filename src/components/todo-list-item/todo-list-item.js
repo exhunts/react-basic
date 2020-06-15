69c0667896dd2df31085a754e4406363840bb921
ExHunts <exemptionhunters@gmail.com>
2020-06-15 06:50:43 +0300

    'to-do-list-item' onClick is done

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,14 +2,51 @@ import React, { Component } from 'react'
 import './todo-list-item.scss'
 
 export default class TodoListItem extends Component {
+  state = {
+    isTaskDone: false,
+    isTaskImportant: false
+  }
+
+  toggleDone = () => {
+    this.setState((state) => ({
+      isTaskDone: state.isTaskDone === true ? false : true
+    }))
+  }
+
+  toggleImportant = () => {
+    this.setState((state) => ({
+      isTaskImportant: state.isTaskImportant === true ? false : true
+    }))
+  }
+
   render() {
-    const { label, important } = this.props
+    const { label } = this.props
+    let itemStyle = ''
+
+    if (this.state.isTaskImportant) {
+      itemStyle += ' important'
+    }
+
+    if (this.state.isTaskDone) {
+      itemStyle += ' done'
+    }
+
     return (
       <li className="todo__item">
-        {label}
+        <span
+          className={itemStyle}
+          onClick={this.toggleDone}
+        >
+          {label}
+        </span>
         <div className="todo__item-control">
           <img className="todo__delete-task" src="images/trash.svg" alt="trash" />
-          <img className="todo__task-done" src="images/danger.svg" alt="exclamation" />
+          <img
+            className="todo__task-done"
+            src="images/danger.svg"
+            alt="exclamation"
+            onClick={this.toggleImportant}
+          />
         </div>
       </li>
     )
